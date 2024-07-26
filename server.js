f9167831d5100c60499a431092fd7e88cfede3d0
jesusdasilva2001 <jesustamba2017@gmail.com>
2024-07-26 23:41:20 +0100

    output message to DOM

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/message');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,10 +11,30 @@ const io = socketio(server)
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const spaceName =  'SyntaxSpace';
+
 // run when client connects
 io.on('connect', socket => {
-    console.log('New ws Connection...')
-})
+    socket.on('joinRoom', ({ username, room }) => {
+        // welcome current user
+    socket.emit('message', formatMessage(spaceName, 'Welcome to SyntaxSpace!'));
+
+    // Broadcast when a user connects
+    socket.broadcast.emit(
+        'message', 
+        formatMessage(spaceName, 'A user has joined the chat'));
+    });
+
+    // listen for chatMessage
+    socket.on('chatMessage', msg => {
+       io.emit('message', formatMessage('USER', msg));
+    });
+
+    // Runs when client disconnect
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(spaceName, 'A user has left the chat'));
+    });
+});
 
 const PORT = 3000 || process.env.PORT;
 
